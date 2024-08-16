5702fe4467324e9cbf15187598a3ab17c4b9121b
Angel Lars <lars50593@gmail.com>
2024-08-15 20:40:18 -0600

    ejemplo con listass terminado

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, agregar, eliminar, editar
 
 urlpatterns = [
     path("", index, name="index"),
+    path("agregar/", agregar, name="agregar" ),
+    path("eliminar/<str:nombre>", eliminar, name="eliminar" ),
+    path("editar/<str:nombre>", editar, name="editar" ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 # Create your views here.
+tareas = []
+
 def index(request):
-    return render(request, 'tasks/index.html')
+    context = {
+        'tareas': tareas
+    }
+    return render(request, 'tasks/index.html', context)
+
+def agregar(request):
+    tarea = request.POST.get('tarea')
+    if tarea:
+        tareas.append(tarea)
+    else:
+        print("Esciba un valor valido!")
+        return redirect('index')
+    return redirect('index')
+
+def eliminar(request, nombre):
+    tareas.remove(nombre)
+    return redirect('index')
+
+def editar(request, nombre):
+    
+    return render(request, "tasks/index.html", {"tarea":nombre, "tareas":tareas})
