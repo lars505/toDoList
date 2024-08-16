54d2a5ffaae9fb9fdf632275a55acf74c41fd546
Angel Lars <lars50593@gmail.com>
2024-08-15 22:02:45 -0600

    ejemplo teminado usando un diccionario

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,6 @@ from .views import index, agregar, eliminar, editar
 urlpatterns = [
     path("", index, name="index"),
     path("agregar/", agregar, name="agregar" ),
-    path("eliminar/<str:nombre>", eliminar, name="eliminar" ),
-    path("editar/<str:nombre>", editar, name="editar" ),
+    path("eliminar/<int:id>", eliminar, name="eliminar" ),
+    path("editar/<int:id>", editar, name="editar" ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,27 +3,33 @@ from django.contrib import messages
 
 
 # Create your views here.
-tareas = []
+# tareas = []
+tareas_dict = {}
 
 def index(request):
     context = {
-        'tareas': tareas
+        'tareas': tareas_dict
     }
     return render(request, 'tasks/index.html', context)
 
 def agregar(request):
-    tarea = request.POST.get('tarea')
-    if tarea:
-        tareas.append(tarea)
-    else:
-        print("Esciba un valor valido!")
+    if request.method == "POST":
+        tarea_id = len(tareas_dict) + 1
+        print(f'Esto es lo que contiene tarea id: {tarea_id}')
+        tarea_text = request.POST.get("tarea")
+        tareas_dict[tarea_id] = { "tarea": tarea_text, "completado": False}
         return redirect('index')
     return redirect('index')
 
-def eliminar(request, nombre):
-    tareas.remove(nombre)
+def eliminar(request, id):
+    if id in tareas_dict:
+        del tareas_dict[id]
     return redirect('index')
 
-def editar(request, nombre):
+def editar(request, id):
+    if request.method == 'POST':
+        tarea = request.POST.get('tarea')
+        tareas_dict[id]['tarea']=tarea
+        return redirect("index")
+    return render(request,'tasks/editar.html', {'tarea_id': id, 'tarea': tareas_dict[id], 'tareas': tareas_dict} )
     
-    return render(request, "tasks/index.html", {"tarea":nombre, "tareas":tareas})
